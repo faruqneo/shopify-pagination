81f659d86c211431e6d096ff9b620de36289a0f5
faruqneo <faruq@marmeto.com>
2019-01-28 17:58:49 +0530

    product and variants update

---
+++ index.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const path = require('path');
+const Shopify = require('shopify-api-node');
+const hbs = require('express-hbs');
+const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const paginate = require('express-paginate');
+var router = express.Router()
+
+//set dotenv values
+const apiKey = process.env.SHOPIFY_PRIVATE_API_KEY;
+const apiPass = process.env.SHOPIFY_PRIVATE_API_PASSWORD;
+const apiSecret = process.env.SHOPIFY_PRIVATE_API_SECRET;
+const shop = process.env.SHOPIFY_PRIVATE_API_SHOP;
+const shopname = process.env.SHOPIFY_PRIVATE_API_SHOP_name;
+
+//Init app
+const app = express();
+
+// keep this before all routes that will use pagination
+app.use(paginate.middleware(10, 50));
+
+
+// Use `.hbs` for extensions and find partials in `views/partials`.
+app.engine('hbs', hbs.express4({
+    partialsDir: __dirname + '/views/partials'
+  }));
+
+//set view engine
+app.set('view engine','hbs');
+app.set('views',path.join(__dirname,'views'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+//shopify API settng
+const shopify = new Shopify({
+    shopName: shopname,
+    apiKey: apiKey,
+    password: apiPass
+  });
+
+// index for testing
+app.get('/',function(req, res){
+   // res.send('Hello');
+   res.render('index',{
+       title: "index",
+       data: "it is working."
+   });
+});
+
+//Product list
+app.get('/shopify_products', function(req, res){
+    shopify.product.list()
+    .then(product => 
+       // res.send(product)
+        res.render('product', {
+          title: "pagination page",
+          product: product
+        })
+    )
+    .catch(err => err)
+});
+
+
+//Details views
+app.get('/product/detail/:id',function(req, res){
+    shopify.product.get(req.params.id)
+    .then(product =>
+        //res.send(product)
+        res.render('details',{
+            title: "detail view",
+            product: product
+        })
+        )
+    .catch(err => console.error(err));
+});
+
+//Edit views
+app.get('/product/edit/:id',function(req, res){
+    shopify.product.get(req.params.id)
+    .then(product => res.render('edit',{
+        title: "edit page",
+        product: product
+    }))
+    .catch(err => console.error(err));
+});
+
+//Edit post
+app.post('/product/edit/:id',function(req, res){
+    let product = req.body;
+    
+    //res.send(product);
+    shopify.product.update(req.params.id, product)
+    .then(product => res.redirect('/shopify_products'))
+    .catch(err => console.error(err));
+});
+
+//Edit varents views
+app.get('/product/variant/edit/:id',function(req, res){
+    shopify.productVariant.get(req.params.id)
+    .then(productVariant => 
+        res.render('variants_edit',{
+            title: "product variants page",
+            variants: productVariant
+        })
+    )
+    .catch(err => console.error(err));
+});
+
+//Edit variants form
+app.post('/product/variant/edit/:id',function(req, res){
+    let productVariant = req.body;
+
+    shopify.productVariant.update(req.params.id, productVariant)
+    .then(productVariant =>res.redirect('/shopify_products'))
+    .catch(err => console.error(err));
+});
+
+
+
+app.listen(3000, function(req, res){
+    console.log("server is running");
+});
